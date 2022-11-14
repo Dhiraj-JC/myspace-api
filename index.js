c7d18138638c01804fe71e70abeb6f523a95071d
Dhiraj-JC <dhiraj@trainitsolutions.com>
2022-11-14 22:19:16 +0530

    feat: created signup API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const { connect } = require('mongoose');
+const authenticateRouter = require('./routes/authenticate');
 
 const app = express();
 dotenv.config();
@@ -20,3 +21,11 @@ connect(
 
 app.use(express.json());
 app.use(cors());
+app.use('/auth',authenticateRouter);
+
+
+app.listen(process.env.PORT, (err)=> {
+
+  if(err) console.log(err);
+  console.log(`Listening on port ${process.env.PORT}`);
+});
